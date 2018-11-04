8331c9180a264d110454207032cfe1a18052afa4
Giovanni Valencia <gvalen9@uic.edu>
2018-11-04 01:38:19 -0500

    TestGui class added

---
+++ MTG Deck Editor/src/TestGUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class TestGUI {
+    private JEditorPane editorPane1;
+    private JPanel panel1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
